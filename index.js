434c33253de4f56aea373d59382e31e82d949f1f
Juan Carlos Medina <juanca.med@gmail.com>
2017-07-11 11:10:10 -0700

    Export deepStateMerge as lib API

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const deepStateMerge = require('./src/deep-state-merge.js');
 const scope = require('./src/scope.js');
 const scopeReducer = require('./src/scope-reducer.js');
 
 module.exports = {
+  deepStateMerge: deepStateMerge,
   scope: scope,
   scopeReducer: scopeReducer,
 };
--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,8 +1,13 @@
+const deepStateMerge = require('../src/deep-state-merge.js');
 const scope = require('../src/scope.js');
 const scopeReducer = require('../src/scope-reducer.js');
 const lib = require('../index.js');
 
 describe('Reusable React Redux', function () {
+  it('exports deepStateMerge', function () {
+    expect(lib.deepStateMerge).toBe(deepStateMerge);
+  });
+
   it('exports scope', function () {
     expect(lib.scope).toBe(scope);
   });
