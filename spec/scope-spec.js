be787f3136fd578cb8a5d47d87cd9f60118b10e0
Juan Carlos Medina <juanca.med@gmail.com>
2017-08-14 18:06:35 -0700

    Improve spec coverage

--- spec/scope-spec.js
+++ spec/scope-spec.js
@@ -9,6 +9,10 @@ const store = {
 };
 
 describe('Reusable React Redux Scope', function () {
+  it('returns the store state given an empty state path', function () {
+    expect(scope(store, [])).toBe(store);
+  });
+
   it('digs into the store state given a state path', function () {
     expect(scope(store, ['a0', 'a1'])).toBe(store.a0.a1);
   });
